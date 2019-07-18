cb3cad8ddaaf1b3db8686401704ca0632e7ba049
giantgingerbreadman <42809862+giantgingerbreadman@users.noreply.github.com>
2019-07-18 14:08:45 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,9 @@
+abra = int(input())
+bulbasaur = int(input())
+charmander = int(input())
+if(abra>bulbasaur and abra>charmander):
+    print(abra)
+elif(bulbasaur>abra and bulbasaur>charmander):
+    print(bulbasaur)
+else:
+    print(charmander)
