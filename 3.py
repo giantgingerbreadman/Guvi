fdf398cf755388fe63bec30ad5168c24b8f1aea9
giantgingerbreadman <42809862+giantgingerbreadman@users.noreply.github.com>
2019-07-18 13:54:43 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,8 +2,7 @@ abra=input()
 if(abra == 'a' or abra == 'e' or abra == 'i' or abra == 'o' or abra == 'u' or abra == 'A' or abra == 'E' or abra == 'I' or abra == 'O' or abra == 'U'):
     print("Vowel")
 else:
-    if(abra.isalpha()) == "True":
+    if(abra.isalpha()== True):
         print("Consonant")
-    else:
+    elif(abra.isalpha()== False):
         print("invalid")
-        
