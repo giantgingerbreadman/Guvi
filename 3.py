e2120d26e54fe6790fead90e8c62705781f88f1f
giantgingerbreadman <42809862+giantgingerbreadman@users.noreply.github.com>
2019-07-18 12:32:44 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,7 @@
+abra=input()
+if(abra == 'a' or abra == 'e' or abra == 'i' or abra == 'o' or abra == 'u' or abra == 'A' or abra == 'E' or abra == 'I' or abra == 'O' or abra == 'U'):
+    print("Positive")
+elif(a==0):
+    print("Zero")
+else:
+    print("Negative")
