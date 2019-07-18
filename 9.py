4b2c4f58675fa41c815f25d5d00c050accf26df5
giantgingerbreadman <42809862+giantgingerbreadman@users.noreply.github.com>
2019-07-18 15:35:04 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,7 +1,9 @@
-abra = int(input())
-bet = int(input())
+a,b = input().split()
+abra = int(a)
+bet = int(b)
 bro = [None] * 100
 sumo = 0 
+int(bet)
 bet = bet + 1
 for x in range(abra):
     bro[x]=x+1
